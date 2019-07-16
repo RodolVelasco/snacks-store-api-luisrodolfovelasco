2d50507ab6f70f4a9101fe1d89c0edc93ba992b2
Rodol <ryurodolfo@gmail.com>
2019-07-16 01:43:24 -0600

    JWT auth try 1

--- snacks-store-api-luisrodolfovelasco/src/main/java/com/applaudo/api/SnackStoreApplication.java
+++ snacks-store-api-luisrodolfovelasco/src/main/java/com/applaudo/api/SnackStoreApplication.java
@@ -3,9 +3,11 @@ package com.applaudo.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableResourceServer
 public class SnackStoreApplication {
 
 	public static void main(String[] args) {
