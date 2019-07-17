aac04f930cbe17bc9f81dd12f04bd85250986bf0
Rodol <ryurodolfo@gmail.com>
2019-07-17 17:41:58 -0600

    Import file character change, create db if not exits.

--- snacks-store-api-luisrodolfovelasco/target/classes/import.sql
+++ snacks-store-api-luisrodolfovelasco/target/classes/import.sql
@@ -1,7 +1,7 @@
 INSERT INTO `products` (`id`, `likes`, `name`, `price`, `quantity`) VALUES(2, 5, 'Quesito', 2, 25);
 INSERT INTO `products` (`id`, `likes`, `name`, `price`, `quantity`) VALUES(3, 3, 'Churrito', 10, 4);
 INSERT INTO `products` (`id`, `likes`, `name`, `price`, `quantity`) VALUES(4, 10, 'Nacho', 2, 20);
-INSERT INTO `products` (`id`, `likes`, `name`, `price`, `quantity`) VALUES(5, 11, 'Jalapeño', 3, 15);
+INSERT INTO `products` (`id`, `likes`, `name`, `price`, `quantity`) VALUES(5, 11, 'Jalapenio', 3, 15);
 INSERT INTO `products` (`id`, `likes`, `name`, `price`, `quantity`) VALUES(6, 50, 'Alboroto', 3, 11);
 INSERT INTO `products` (`id`, `likes`, `name`, `price`, `quantity`) VALUES(7, 10, 'Tortrix', 1, 5);
 INSERT INTO `products` (`id`, `likes`, `name`, `price`, `quantity`) VALUES(8, 10, 'Tocinito', 1, 5);
